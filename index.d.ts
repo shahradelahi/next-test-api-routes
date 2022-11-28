b8ebf5d9a700b4f17be61b9e178e3038ad2d3ff7
Shahrad Elahi <shahradq@gmail.com>
2022-11-28 05:44:51 +0330

    Updated the `type declarations`

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-declare const createMocks: any;
+export declare const createMocks: any;
 /**
  * Parse the response
  *
  * @param {Object} response
  * @return {{headers: Headers, body: any, statusCode: number}}
  */
-declare function parseResponse(response: any): {
+export declare function parseResponse(response: any): {
    statusCode: any;
    headers: any;
    body: any;
